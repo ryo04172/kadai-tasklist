6db60a7e1086e1b05cb2e7806d1a276468e21789
y_ryo <2ryoryoryo40172@gmail.com>
2021-02-21 09:07:18 +0000

    resubmit
    ;
    qqqq
    ;
    :

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,11 +47,10 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
 
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
 
-end
-
-private
-
-def task_params
-  params.require(:task).permit(:content)
 end
