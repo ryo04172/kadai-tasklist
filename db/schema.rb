c9cb9ee978d14ad3aa171034e3b8729474768f9d
y_ryo <2ryoryoryo40172@gmail.com>
2021-03-06 08:10:29 +0000

    comp_210306

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_151311) do
+ActiveRecord::Schema.define(version: 2021_03_06_080603) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "user_id_id"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
+    t.index ["user_id_id"], name: "index_tasks_on_user_id_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
